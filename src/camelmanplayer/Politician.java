5d9dbcb1b8a26ab6f9344899b8dc9b96d863be52
Angela Loh <angelaloh09@gmail.com>
2021-01-11 15:34:57 -0600

    politician

--- src/camelmanplayer/Politician.java
+++ src/camelmanplayer/Politician.java
@@ -27,19 +27,4 @@ public class Politician extends RobotPlayer{
             }
         }
     }
-
-
-    void universalPrinciple() throws GameActionException {
-        // try move
-        Team enemy = rc.getTeam().opponent();
-        int actionRadius = rc.getType().actionRadiusSquared;
-        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
-        if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
-            System.out.println("empowering...");
-            rc.empower(actionRadius);
-            System.out.println("empowered");
-            return;
-        }
-    }
-
 }
